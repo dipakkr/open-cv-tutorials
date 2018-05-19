64f84fbfbb8abd8400f08dba8de8d3db92e727a9
Deepak <raj.deeepak@gmail.com>
2018-05-20 01:30:28 +0530

    image load and show added

---
+++ lesson-1.py
@@ -0,0 +1,18 @@
+import cv2
+
+image = cv2.imread('./deepak.jpeg')
+cv2.imshow('Original',image)
+cv2.waitKey(1000)
+
+#destroy the window with window name original
+cv2.destroyWindow('Original')
+
+#Convert into grey scale
+grey_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+cv2.imshow('Grey Scaled',grey_image)
+cv2.waitKey(1000)
+
+grey_image2 = cv2.imread('./deepak.jpeg',0)
+cv2.imshow('Grayscale',grey_image2)
+cv2.waitKey(1000)
+
